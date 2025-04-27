7e791366314f57d95de28e8776af0bb6d8a43666
FARID-NR <nurfarid776@gmail.com>
2025-04-27 17:03:33 +0800

    create CategoryRepositori

---
+++ src/main/java/com/be/ecommerce/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.be.ecommerce.repositories;
+
+import com.be.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
